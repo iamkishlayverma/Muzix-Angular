03a76cb6b3bbb0cf7ec3d2bda61293f1f9caba1d
iamkishlayverma <verma.kishlay688@gmail.com>
2020-12-30 14:41:23 +0530

    Changes in App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeleteTrackComponent } from './delete-track/delete-track.component';
+import { GetAllTracksComponent } from './get-all-tracks/get-all-tracks.component';
+import { HomeComponent } from './home/home.component';
+import { LastFmComponent } from './last-fm/last-fm.component';
+import { SaveTrackComponent } from './save-track/save-track.component';
+import { SearchTrackComponent } from './search-track/search-track.component';
+import { UpdateTrackComponent } from './update-track/update-track.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchTrackComponent },
+  { path: 'searchtrack/:trackname', component: LastFmComponent },
+  { path: 'savetrack/:mbid', component: SaveTrackComponent },
+  { path: 'myplaylist', component: GetAllTracksComponent },
+  { path: 'deletetrack/:mbid', component: DeleteTrackComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'updatetrack/:mbid/:comment', component: UpdateTrackComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchTrackComponent } from './search-track/search-track.component';
@@ -11,7 +10,6 @@ import { LastFmComponent } from './last-fm/last-fm.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MuzixHttpService } from './muzix-http.service';
-import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { GetAllTracksComponent } from './get-all-tracks/get-all-tracks.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -35,17 +33,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
-    NgxSpinnerModule,
-    RouterModule.forRoot([
-      {path: 'search', component: SearchTrackComponent},
-      {path: 'searchtrack/:trackname', component: LastFmComponent},
-      {path: 'savetrack/:mbid', component: SaveTrackComponent},
-      {path: 'myplaylist', component: GetAllTracksComponent},
-      {path: 'deletetrack/:mbid', component: DeleteTrackComponent},
-      {path: 'home', component: HomeComponent},
-      {path: 'updatetrack/:mbid/:comment', component: UpdateTrackComponent},
-      {path: '', redirectTo: '/home', pathMatch: 'full'}
-    ])
+    NgxSpinnerModule
   ],
   providers: [MuzixHttpService],
   bootstrap: [AppComponent]
